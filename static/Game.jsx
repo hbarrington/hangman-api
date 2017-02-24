c29da932399e2bc657d5036ee333aec117573892
Hunter Barrington <hbarrin@gmail.com>
2017-02-24 13:33:33 -0500

    allow user to select a game and display it's progress

--- static/Game.jsx
+++ static/Game.jsx
@@ -10,6 +10,12 @@ class Game extends React.Component {
         <h4 className="number-of-guesses">
           {"Number of Guesses " + this.props.number_of_guesses}
         </h4>
+        <h4 className="partial-word">
+          {this.props.partial_word}
+        </h4>
+        <h4 className="wrong-guesses">
+          {this.props.wrong_guesses}
+        </h4>
         <h4 className="date-created">
           {"Created: " + this.props.created_date}
         </h4>
@@ -19,6 +25,3 @@ class Game extends React.Component {
 };
 
 export default Game;
-
-// import ReactDOM from 'react-dom';
-// ReactDOM.render(<Game _id='123456789' number_of_guesses='6' created_date='1-2-16'/>, document.getElementById('games'));
--- static/GameList.jsx
+++ static/GameList.jsx
@@ -3,41 +3,63 @@ import ReactDOM from 'react-dom';
 import Game from './Game.jsx';
 var $ = require("jquery");
 
+function GameListing(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props._id} - {props.number_of_guesses} guesses - {props.created_date}
+    </button>
+  );
+}
+
 class GameList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {games: []};
+    this.state = {games: [], current_game: null};
   }
 
   componentDidMount() {
-    this.GameList();
+    this.getGames();
   }
 
-  GameList(){
+  getGames(){
     return $.getJSON('http://localhost:8080/games/')
       .then((data) => {
-        console.log(data)
         this.setState({ games: data });
       });
   }
 
+  getGame(id){
+    return $.getJSON('http://localhost:8080/games/'+id)
+      .then((data) => {
+        let game = <Game
+          _id={data._id}
+          number_of_guesses={data.number_of_guesses}
+          created_date={data.created_date}
+          partial_word={data.partial_word}
+          wrong_guesses={data.wrong_guesses}
+          />
+        this.setState({ current_game: game})
+      });
+  }
 
   render() {
+    let games = this.state.games.map((game) => {
+     return <li key={game._id}> <GameListing _id={game._id} number_of_guesses={game.number_of_guesses} created_date={game.created_date} onClick={() => this.getGame(game._id)}/> </li>
+    });
 
-      let games = this.state.games.map((game) => {
-       return <li> <Game _id={game._id} number_of_guesses={game.number_of_guesses} created_date={game.created_date}/> </li>
-      });
-
-      return (
-     <div className="gameBox">
+    return (
+     <div className="gameListBox">
        <h1>Available Games</h1>
        <div className="gameList">
          {games}
        </div>
+       <div className="currentGame">
+         {this.state.current_game}
+       </div>
      </div>
-   );
-
+    );
   }
+
 };
 
 
